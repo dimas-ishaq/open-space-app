3ca9a745cd77ce11b30addbae9466c1af9862f88
Dimas Maulana <masjamad2@gmail.com>
2024-04-14 09:06:37 +0700

    latihan CI/CD Dicoding

---
+++ src/components/LoginInput.test.jsx
@@ -0,0 +1,34 @@
+/**
+ * skenario testing
+ *
+ * - LoginInput component
+ *   - should handle username typing correctly
+ *   - should handle password typing correctly
+ *   - should call login function when login button is clicked
+ */
+
+import React from 'react';
+import { describe, it, expect } from 'vitest';
+
+import { render, screen } from '@testing-library/react';
+import LoginInput from './LoginInput';
+
+import userEvent from '@testing-library/user-event';
+import matchers from '@testing-library/jest-dom/matchers';
+
+expect.extend(matchers);
+
+describe('LoginInput component', () => {
+  it('should handle username typing correctly', async () => {
+    // Arrange
+    render(<LoginInput login={() => { }} />);
+    const usernameInput = await screen.getByPlaceholderText('Username');
+
+    // Action
+    await userEvent.type(usernameInput, 'usernametest');
+
+    // Assert
+
+    expect(usernameInput).toHaveValue('usernametest');
+  })
+});
