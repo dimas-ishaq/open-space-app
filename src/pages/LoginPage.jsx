a335e2a46757a9c885e27b60427a26d250cd065d
Dimas Maulana <masjamad2@gmail.com>
2024-04-14 09:43:00 +0700

    make test pass

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginInput from '../components/LoginInput';
 import { asyncSetAuthUser } from '../states/authUser/action';
- 
+
 function LoginPage() {
   const dispatch = useDispatch(); // @TODO: get dispatch function from store
- 
+
   const onLogin = ({ id, password }) => {
     // @TODO: dispatch async action to login
     dispatch(asyncSetAuthUser({ id, password }));
   };
- 
+
   return (
     <section className="login-page">
       <header className="login-page__hero">
@@ -26,9 +26,9 @@ function LoginPage() {
           ,
           {' '}
           <br />
-          Through Open Space.
+          Through Open Space App.
         </h2>
- 
+
         <LoginInput login={onLogin} />
         <p>
           Don&apos;t have an account?
@@ -39,5 +39,5 @@ function LoginPage() {
     </section>
   );
 }
- 
+
 export default LoginPage;
